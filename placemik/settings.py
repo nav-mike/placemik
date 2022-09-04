74f822313a894470d279415927026b623dc088b9
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-09-04 11:39:31 +0300

    config postgresql

--- placemik/settings.py
+++ placemik/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "placemik.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "placemik",
+        "USER": "placemik",
+        "PASSWORD": "placemik",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
@@ -88,9 +92,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
