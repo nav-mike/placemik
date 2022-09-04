a11c87baebf0521bbfa02033faf95698d4578c17
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-09-04 12:04:06 +0300

    add category and product models

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    image_url = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
