6700a6446c79472a25ef0753faf37818fc7cbc0a
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-09-04 12:15:30 +0300

    add product detail view

--- webapp/views.py
+++ webapp/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the placemik index.")
+
+
+def detail(request: HttpRequest, product_id: int) -> HttpResponse:
+    return HttpResponse("You're looking at product %s." % product_id)
