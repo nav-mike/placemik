2ec60ad0a0bb3e95004f62c1a6ae1c24a24721a0
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-09-08 11:51:35 +0300

    add namespace for url

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "webapp"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:category_id>/", views.detail, name="detail"),
