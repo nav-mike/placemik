eb95849270c02cff26fb5815e0862501b66339ad
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-09-08 14:41:00 +0300

    add test example

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SampleTest(TestCase):
+    def test_sample(self):
+        self.assertEqual(1, 1)
+
+    def test_wrong(self):
+        self.assertEqual(1, 2)
