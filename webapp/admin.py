38cee2dbb1854ed1826c18d6368e2e623d1156d8
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-09-12 17:43:38 +0300

    add csv import

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Category, Product
 
-admin.site.register(Category)
+
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+
+
+class CategoryAdmin(ImportExportModelAdmin):
+    resource_class = CategoryResource
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product)
