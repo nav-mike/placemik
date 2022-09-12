92705c582ad44a333dc44cd419e038aa956dbc3f
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-09-12 17:48:42 +0300

    add category group

--- webapp/models.py
+++ webapp/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 
+class CategoryGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    group = models.ForeignKey(
+        CategoryGroup, on_delete=models.CASCADE, null=True, blank=True
+    )
 
     def __str__(self):
         return self.name
