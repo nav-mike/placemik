d6a7d2effe6a23cba8cff42abc2946cf6fb4542a
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-21 16:37:04 +0200

    add search form

--- theme/forms.py
+++ theme/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from webapp.models.category_group import CategoryGroup
+
 
 class NewsletterForm(forms.Form):
     email = forms.EmailField(
@@ -8,6 +10,28 @@ class NewsletterForm(forms.Form):
             attrs={
                 "placeholder": "Enter your email here",
                 "class": "max-w-sm w-96 bg-transparent border-white placeholder-white placeholder:italic",
+            }
+        ),
+    )
+
+
+class TextSearchForm(forms.Form):
+    categoryGroup = forms.ChoiceField(
+        label="",
+        choices=[(0, "All categories")]
+        + [(group.id, group.name) for group in CategoryGroup.objects.all()],
+        widget=forms.Select(
+            attrs={
+                "class": "border-0 h-full bg-purple-100",
+            }
+        ),
+    )
+    search = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search",
+                "class": "flex-1 focus:outline-0 px-4 border-0",
             }
         ),
     )
--- webapp/context_processors.py
+++ webapp/context_processors.py
@@ -1,9 +1,10 @@
 from webapp.models import CategoryGroup
-from theme.forms import NewsletterForm
+from theme.forms import NewsletterForm, TextSearchForm
 
 
 def categories(_request):
     return {
         "categoryGroups": CategoryGroup.objects.all().prefetch_related("categories"),
         "newsletterForm": NewsletterForm(),
+        "textSearchForm": TextSearchForm(),
     }
