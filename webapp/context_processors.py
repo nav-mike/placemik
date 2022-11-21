81b04e18c33d73693072162e6d9a64a35e78be46
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-21 16:50:02 +0200

    fix search by search form

--- webapp/context_processors.py
+++ webapp/context_processors.py
@@ -2,9 +2,9 @@ from webapp.models import CategoryGroup
 from theme.forms import NewsletterForm, TextSearchForm
 
 
-def categories(_request):
+def categories(request):
     return {
         "categoryGroups": CategoryGroup.objects.all().prefetch_related("categories"),
         "newsletterForm": NewsletterForm(),
-        "textSearchForm": TextSearchForm(),
+        "textSearchForm": TextSearchForm(initial=request.GET),
     }
--- webapp/views/products/index_view.py
+++ webapp/views/products/index_view.py
@@ -15,6 +15,20 @@ class IndexView(ListView):
 
         if self.request.GET.get("category"):
             queryset = queryset.filter(category_id=int(self.request.GET["category"]))
+        elif self.request.GET.get("categoryGroup") and self.request.GET.get("search"):
+            if self.request.GET.get("categoryGroup") != "0":
+                queryset = queryset.filter(
+                    category__group_id=self.request.GET["categoryGroup"],
+                    name__icontains=self.request.GET["search"],
+                )
+            else:
+                queryset = queryset.filter(
+                    name__icontains=self.request.GET["search"],
+                )
+
+        # import pdb
+
+        # pdb.set_trace()
 
         return queryset
 
@@ -25,5 +39,7 @@ class IndexView(ListView):
             context["category"] = Category.objects.get(
                 pk=int(self.request.GET["category"])
             )
+        elif self.request.GET.get("categoryGroup") and self.request.GET.get("search"):
+            context["search"] = self.request.GET.get("search")
 
         return context
