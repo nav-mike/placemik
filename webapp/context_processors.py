0668938c533c262a9b168bbd869a632592f113a7
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-24 14:31:06 +0200

    show carts in base template

--- webapp/context_processors.py
+++ webapp/context_processors.py
@@ -1,5 +1,8 @@
+from ctypes import Array
+from django.http import HttpRequest
 from webapp.models import CategoryGroup
 from theme.forms import NewsletterForm, TextSearchForm
+from webapp.models import OrderItem, Product
 
 
 def categories(request):
@@ -7,4 +10,27 @@ def categories(request):
         "categoryGroups": CategoryGroup.objects.all().prefetch_related("categories"),
         "newsletterForm": NewsletterForm(),
         "textSearchForm": TextSearchForm(initial=request.GET),
+        "cart": cart(request),
     }
+
+
+def cart(request: HttpRequest) -> Array[OrderItem]:
+    cart = request.session.get("cart", {})
+
+    if not cart:
+        return []
+
+    result = []
+    products = Product.objects.filter(pk__in=list(cart.keys()))
+
+    for key, value in cart.items():
+        product = products.get(pk=key)
+        result.append(
+            OrderItem(
+                product=product,
+                quantity=value,
+                price=product.price * value,
+            )
+        )
+
+    return result
